b1bbfcae4724c00b746b6bb0d94ac6cde06a44a0
anbarsaleem <anbar.saleem@gmail.com>
2020-08-16 01:23:16 -0400

    Organized intro section and styled navbar buttons (unfinished)

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,17 +1,20 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {stickynavbar()};
+$(document).ready(function() {
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {stickynavbar()};
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+  // Get the navbar
+  var navbar = document.getElementById("navbar");
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickynavbar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function stickynavbar() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
   }
-}
+});
