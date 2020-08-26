58552c711c6aaf9efddcbffea2cadf7a14d17143
anbarsaleem <anbar.saleem@gmail.com>
2020-08-26 01:36:40 -0400

    fixed issues with projects not being toggled with button

--- resources/js/projects-script.js
+++ resources/js/projects-script.js
@@ -1,22 +1,18 @@
-$(document).ready(function() {
+function toggleProject(projectNum) {
 
-  function toggleProject(projectNum) {
+  var projId = "project_button_" + projectNum;
+  var x = document.getElementById(projId);
 
-    var projId = "project_button_" + projectNum;
-    var x = document.getElementById(projId);
+  if(x.style.display === "none" || x.style.display === "") {
 
-    if(x.style.display === "none" || x.style.display === "") {
-
-        x.style.display = "flex";
-
-    }
-
-    else {
-
-        x.style.display = "none";
-
-    }
+      x.style.display = "flex";
 
   }
-  
-});
+
+  else {
+
+      x.style.display = "none";
+
+  }
+
+}
